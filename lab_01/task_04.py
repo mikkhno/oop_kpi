6bfb2bda951b8de8170d9a661a1bb060a29063f7
mikkhno <100687196+mikkhno@users.noreply.github.com>
2022-10-05 01:06:15 +0200

    changed the algorithm to iterative, but don't get this task absolutely

--- lab_01/task_04.py
+++ lab_01/task_04.py
@@ -2,49 +2,46 @@ import argparse
 
 parser = argparse.ArgumentParser(description='knapsack_game')
 parser.add_argument('-W', '--capacity', type=int)
-parser.add_argument('-w', '--weight',  type=int, nargs='+')
-parser.add_argument('-n', '--amount',  type=int)
+parser.add_argument('-w', '--weight', type=list, nargs='+')
+parser.add_argument('-n', '--amount', type=int)
 args = parser.parse_args()
 
-n = args.amount
-c = args.capacity
+
+def knapsack(n, w, c):
+    """
+    :param int n: number of items
+    :param list w: weights of items
+    :param int c: capacity of knapsack
+    """
+    # list_e = [[0 for x in range(n)] for y in range(c)]
+    #
+    # for i in range(1, n + 1):
+    #     for a in range(1, c + 1):
+    #         if w[i] > a:
+    #             list_e[i][a] = list_e[i - 1][a]
+    #         else:
+    #             if (w[i] + list_e[i - 1][c - w[i]]) > list_e[i - 1][a]:
+    #                 list_e[i][a] = w[i] + list_e[i - 1][a - w[i]]
+    #             else:
+    #                 list_e[i][a] = list_e[i - 1][a]
+
+    # return list_e[n][a]
+
+    max_weight = [[w[x] for x in range(n)] for y in range(c)]
+
+    for i in range(1, n + 1):
+        for k in range(1, c + 1):
+            if max_weight[i][k] > c:
+                max_weight[i][k] = max_weight[i - 1][k]
+            else:
+                if (max_weight[i][k] + max_weight[i - 1][c - max_weight[i][k]]) > max_weight[i - 1][k]:
+                    max_weight[i][k] = max_weight[i][k] + max_weight[i - 1][c - max_weight[i][k]]
+                else:
+                    max_weight[i][k] = max_weight[i - 1][k]
+
+    return max_weight[i][k]
 
 
-def table_of_performing(n, c):
-
-    table = [n][c]
-    #w - maximal weight
-    w = [n]
-    result = None
-
-    if table[n][c]:
-        return table[n][c]
-
-    if not n or not c:
-        result = 0
-    else:
-        if w[n] > c:
-            result = table_of_performing(n-1, c)
-
-        else:
-            tmp1 = table_of_performing(n-1, c)
-            tmp2 = table_of_performing(n-1, c - w[n])
-            result = max(tmp1, tmp2)
-
-        result = table[n][c]
-        return result
-
-    if __name__ == '__main__':
-        print(table_of_performing(n, c))
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    w = [5, 3, 1, 7]
+    print(knapsack(4, w, 10))
