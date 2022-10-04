5f4d644ae4b772ec75b5510d5f15f0cbc3236ff2
mikkhno <100687196+mikkhno@users.noreply.github.com>
2022-10-04 22:40:36 +0200

    added checking of entered mathematics operations

--- lab_01/task_02.py
+++ lab_01/task_02.py
@@ -2,17 +2,26 @@ import argparse
 import math
 import operator
 
-parser = argparse.ArgumentParser(description="Calculating universal math operations")
-parser.add_argument('operators', type=str)
-parser.add_argument('num1', type=float)
-parser.add_argument('num2', type=float)
-args = parser.parse_args()
 
-performing = getattr(math, args.operators, None)
-if performing:
-    performing_res = getattr(math, args.operators)
-else:
-    performing_res = getattr(operator, args.operators)
+def math():
+    parser = argparse.ArgumentParser(description="Calculating universal math operations")
+    parser.add_argument('operators', type=str)
+    parser.add_argument('num1', type=float)
+    parser.add_argument('num2', type=float)
+    args = parser.parse_args()
 
-print(performing_res(args.num1, args.num2))
+    performing = getattr(math, args.operators, None)
+
+    if not performing:
+        performing = getattr(operator, args.operators, None)
+
+    if not performing:
+        print('Math operation has not been detected.')
+        return None
+
+    print(performing(args.num1, args.num2))
+
+
+if __name__ == '__main__':
+    math()
 
