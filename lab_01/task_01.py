1b591c1e243b789e2139e1e99e1e2355583e460e
mikkhno <100687196+mikkhno@users.noreply.github.com>
2022-10-05 20:40:24 +0200

    created a new folder

--- lab_01/task_01.py
+++ lab_01/task_01.py
@@ -25,6 +25,20 @@ def math_operation(num1, operation, num2):
         return None
 
 
-if __name__ == '__main__':
+def main():
+    if args.operation == '/' and (args.num1 == 0 or args.num2 == 0):
+        print('Dividing by zero.')
+        return 0
+
+    math_operation(args.num1, args.operation, args.num2)
+
+    if not math_operation(args.num1, args.operation, args.num2):
+        print('Missing operation.')
+        return 0
+    else:
         print(math_operation(args.num1, args.operation, args.num2))
+
+
+if __name__ == '__main__':
+        main()
 
