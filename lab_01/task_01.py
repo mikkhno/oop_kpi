54af4c6058a7102fbb21d45f968cd88f373e736e
mikkhno <100687196+mikkhno@users.noreply.github.com>
2022-10-06 09:18:51 +0200

    added comments

--- lab_01/task_01.py
+++ lab_01/task_01.py
@@ -1,5 +1,6 @@
 import argparse
 
+# using argparse to receive arguments from shell and then parse
 parser = argparse.ArgumentParser(description='Calculating simple math operations')
 parser.add_argument('num1', type=float)
 parser.add_argument('operation', type=str)
@@ -8,7 +9,6 @@ args = parser.parse_args()
 
 
 def math_operation(num1, operation, num2):
-
     if operation == '+':
         return num1 + num2
 
@@ -21,24 +21,26 @@ def math_operation(num1, operation, num2):
     if operation == '/':
         return num1 / num2
 
+    # in this case we are returning None to show unrecognised operation in the next function
     else:
         return None
 
 
 def main():
+    # we are checking if there is no dividing by zero
     if args.operation == '/' and (args.num1 == 0 or args.num2 == 0):
         print('Dividing by zero.')
         return 0
 
     math_operation(args.num1, args.operation, args.num2)
 
+    # if we have unrecognised operation, this condition will inform us, otherwise showing the results
     if not math_operation(args.num1, args.operation, args.num2):
-        print('Missing operation.')
+        print('Unrecognised operation.')
         return 0
     else:
         print(math_operation(args.num1, args.operation, args.num2))
 
 
 if __name__ == '__main__':
-        main()
-
+    main()
