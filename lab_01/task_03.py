fccbf4d1649fad2e0688b9edb805f254a581543c
mikkhno <100687196+mikkhno@users.noreply.github.com>
2022-10-06 10:35:30 +0200

    added comments

--- lab_01/task_03.py
+++ lab_01/task_03.py
@@ -5,15 +5,18 @@ parser.add_argument('f_string', type=str)
 args = parser.parse_args()
 
 
+# bool function for validating
 def checking():
-
+    # adding recognisable signs as operators
     signs = ('+', '-')
     previous_symbol = ''
+    # adding bool for validating the string
     is_valid = True
 
+    # checking if symbol is numeric or sign, order and if it is not the end of string
     if args.f_string:
         for symbol in args.f_string:
-            if symbol.isnumeric() or (symbol in signs and previous_symbol.isnumeric() and symbol !=args.f_string[-1]):
+            if symbol.isnumeric() or (symbol in signs and previous_symbol.isnumeric() and symbol != args.f_string[-1]):
                 previous_symbol = symbol
             else:
                 is_valid = False
