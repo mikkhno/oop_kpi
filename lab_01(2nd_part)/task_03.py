8c1060e2ac4d161b16de8eac078f6ef8080f0a41
mikkhno <100687196+mikkhno@users.noreply.github.com>
2022-10-13 06:54:56 +0200

    adding last methods

--- lab_01(2nd_part)/task_03.py
+++ lab_01(2nd_part)/task_03.py
@@ -1,9 +1,31 @@
 class Product:
 
-    def __init__(self, price, description, quantity):
+    def __init__(self, name, price, description, quantity):
+        self.name = name
         self.price = price
         self.description = description
         self.dimension = quantity
+
+        @property
+        def price(self):
+            return self.__price
+
+        @price.setter
+        def price(self, value):
+            if not isinstance(value, int | float):
+                raise ValueError('Price is not a number.')
+            if not value:
+                raise ValueError('Price cannot be equal 0.')
+            if value < 0:
+                raise ValueError('Price cannot be a negative number.')
+            self.__price = value
+
+        @price.getter
+        def price(self):
+            return self.__price
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 class Customer:
@@ -13,35 +35,41 @@ class Customer:
         self.patronymic = patronymic
         self.mobile = mobile
 
+    def __str__(self):
+        return f'{self.name} {self.surname}'
+
 
 class Order:
 
-    def __init__(self, customer, products_list):
+    def __init__(self, customer):
         self.cart_items = []
         self.summary = 0.0
         self.customer = customer
-        self.products_list = products_list
 
     def add_item(self, item_name, item_price, item_quantity):
-        for x in item_quantity:
+        for x in range(item_quantity):
             self.cart_items.append(item_name)
             self.summary += item_price
 
     def summarising(self):
-        shop_list = dict((item, self.cart_items.count(item)) for item in self.cart_items)
-        return shop_list
+        cart_items = dict((item, self.cart_items.count(item)) for item in self.cart_items)
+        return cart_items
+
+    def __str__(self):
+        self.products_list = self.summarising()
+        return f'{self.customer} bought {self.summarising()}' \
+            f'\nIn total: {self.summary}'
 
 
 def main():
     user1 = Customer('Ternov', 'Stepan', 'Mykhailovych', '53600')
-    apple = Product(3.59, 'tasty', 10)
-    pear = Product(2.99, 'awful', 10)
-    user1_order = Order(user1, products_list='')
-    user1_order.add_item(apple, 3.59, 3)
-    user1_order.add_item(pear, 2.99, 1)
-    user1_order.products_list = user1_order.summarising()
-    print(user1_order.products_list)
-    print('Total: ', user1_order.summary)
+    apple = Product('apple', 3.59, 'tasty', 10)
+    pear = Product('apple', 2.99, 'awful', 10)
+    order1 = Order(user1)
+    order1.add_item(apple, 3.59, 3)
+    order1.add_item(pear, 2.99, 1)
+    print(order1)
 
-    if __name__ == '__main__':
-        main()
+
+if __name__ == '__main__':
+    main()
