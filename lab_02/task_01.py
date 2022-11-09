d442866e55958f794e726a78320a7b0f79ce2b46
mikkhno <100687196+mikkhno@users.noreply.github.com>
2022-11-09 19:12:09 +0100

    finally done

--- lab_02/task_01.py
+++ lab_02/task_01.py
@@ -13,7 +13,13 @@ s = 'F'
 # ticket type
 tt = ['Normal', 'Advanced', 'Student', 'Late']
 
-price = 100
+price = 625
+
+
+def option_display():
+    for i in range(len(tt)):
+        print(i + 1, ' - ', tt[i])
+
 
 class NormalTicket:
     def __init__(self, price, ttype):
@@ -47,49 +53,53 @@ class NormalTicket:
             print(time[i], end=':')
         print()
 
-
     def __str__(self):
         self.sgen()
         self.sn_reader(self.ser_num)
-        return f'SerialNumber:{self.ser_num}\nPrice:{self.price}'
+        return f'SerialNumber:{self.ser_num}\nPrice: {self.price} ₴'
 
 
 class AdvancedTicket(NormalTicket):
-    def __init__(self, price, ttype):
+    def __init__(self, price, ttype, disc=0.6):
         super().__init__(price, ttype)
-        self.price = 0.6 * self.price
+        self.price = disc * self.price
         self.ttype = 'Advanced'
 
 
 class StudentTicket(NormalTicket):
-    def __init__(self, price, ttype):
-        super().__init__(price, ttype)
-        self.price = 0.5 * self.price
+    def __init__(self, price, ttype, disc=0.5):
+        super().__init__(price, ttype, )
+        self.price = disc * self.price
         self.ttype = 'Student'
 
 
 class LateTicket(NormalTicket):
-    def __init__(self, price, ttype):
+    def __init__(self, price, ttype, sanc=1.1):
         super().__init__(price, ttype)
-        self.price = 1.1 * self.price
+        self.price = sanc * self.price
         self.ttype = 'Late'
 
 
-choice = int(input("Which ticket do you want to buy?\n1 - Normal  2 - Advanced  3 - Student  4 - Late\n"))
-match choice:
-    case 1:
-        ticket = NormalTicket(price,'Normal')
-    case 2:
-        ticket = AdvancedTicket(price,'Normal')
-    case 3:
-        ticket = StudentTicket(price,'Normal')
-    case 4:
-        ticket = LateTicket(price,'Normal')
-    case _:
-        print('Your choice has not been recognised.')
+def main():
+    option_display()
+    choice = int(input("Which ticket do you want to buy?\n"))
+    match choice:
+        case 1:
+            ticket = NormalTicket(price, 'Normal')
+        case 2:
+            ticket = AdvancedTicket(price, 'Normal')
+        case 3:
+            ticket = StudentTicket(price, 'Normal')
+        case 4:
+            ticket = LateTicket(price, 'Normal')
+        case _:
+            print('Your choice has not been recognised.')
+            return 0
 
-print("Take your ticket and enjoy the IT-event!")
-print(ticket)
+    print("-----T I C K E T -----")
+    print(ticket)
+    print("Take your ticket and enjoy the IT-event!")
 
 
-
+if __name__ == "__main__":
+    main()
